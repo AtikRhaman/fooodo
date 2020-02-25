e0068b56a2a31b54b1121de051c5e5b91ea78ec5
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2020-02-25 21:20:58 +0200

    food-cart

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,9 @@
 import React from "react";
-
-const Main = () => {
-  return <h1>Main content Gose here</h1>;
-};
+import FoodCart from "./FoodCart";
+import FoodPopular from './FoodPopular'
+const Main = () =>(
+  <div>
+    <FoodCart/>
+    <FoodPopular/>
+  </div>);
 export default Main;
